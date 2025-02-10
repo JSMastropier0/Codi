4d7b78f9e3bda0ffce380ab819e103d384ffc81e
admin_asis <admin_asis@i2cat.net>
2025-02-10 19:27:03 +0100

    Afegeixo bluetooth per C3

---
+++ RC_BLUETOOTH_SERVOS_ModelPerVendre/RC_BLUETOOTH_SERVOS_ModelPerVendre.ino
@@ -0,0 +1,102 @@
+#include "BluetoothSerial.h"
+#include <esp_now.h>
+#include <ESP32Servo.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+const int Mesq = 2; //M1 motor esquerre
+const int Mdrt = 10; //M2 motor dret
+const int V = 135; //angle per anar endavant
+const int R = 45; //angle per anar endarrere
+
+const char nn='W'; //endavant
+const char ss='S'; //endarrere
+const char oo='A'; //esquerra
+const char ee='D'; //dreta
+const char st='X'; //para
+
+//Servo
+
+Servo servoMotorl; // servo esquerre
+Servo servoMotorr; // servo dret
+
+void endavant (){
+  servoMotorl.write(R);
+  servoMotorr.write(V);
+}
+
+void endarrere (){
+  servoMotorl.write(V);
+  servoMotorr.write(R);
+}
+
+void dreta (){
+  servoMotorl.write(R);
+  servoMotorr.write(90);
+}
+
+void esquerra (){
+  servoMotorl.write(90);
+  servoMotorr.write(R);
+}
+
+void atura (){
+  servoMotorl.write(90);
+  servoMotorr.write(90);
+}
+  
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Consola activada.");
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  pinMode(Mesq,OUTPUT);
+  pinMode(Mdrt,OUTPUT);
+  servoMotorl.attach(Mesq); 
+  servoMotorr.attach(Mdrt); 
+ 
+}
+
+void loop() {
+  
+  if (SerialBT.available()) {
+    char dades=SerialBT.read();
+    Serial.write(dades);
+    Serial.write("\n");
+    switch (dades){ 
+      case nn: 
+        endavant();
+        Serial.write("Endavant");
+        Serial.write("\n");
+        break;        
+      case ss: 
+        endarrere();
+        Serial.write("endarrere");
+        Serial.write("\n");
+        break;
+      case oo: 
+        esquerra();
+        Serial.write("esquerra");
+        Serial.write("\n");
+        break;
+      case ee: 
+        dreta();
+        Serial.write("dreta");
+        Serial.write("\n");
+        break;
+      case st: 
+        atura();
+        Serial.write("atura");
+        Serial.write("\n");
+        break;
+    
+    }
+  }
+  delay(20);
+}
